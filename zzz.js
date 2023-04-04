5ad700f1755b8e0ff6247adb458bc97585368431
Bbenjamil <bbenjamill@icloud.com>
2023-04-05 00:11:19 +0100

    JS class 1

---
+++ zzz.js
@@ -0,0 +1,22 @@
+// alert("hello world")
+
+// let a = 1
+// console.log(a);
+//  a = 7 
+//  let b = 2
+//  let c = a+b 
+//  console.log(c);
+// console.log("hello xanny");
+
+// const z = 8
+
+// console.log(z);
+// var y = 12
+// y = 30
+// console.log(y);
+// let a = "ben" 
+// console.log(a);
+const firstName = "James"
+const firstname = "Stephen"
+console.log(firstName);
+console.log(firstname);
